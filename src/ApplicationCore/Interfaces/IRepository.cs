bf8e57494c50ad8cfeeaa0c63e1e8d88b6903bbe
Akın Özçelik <110735711+akinozcelik0@users.noreply.github.com>
2023-02-17 16:07:02 +0300

    IRepository Created.

--- src/ApplicationCore/Interfaces/IRepository.cs
+++ src/ApplicationCore/Interfaces/IRepository.cs
@@ -1,4 +1,5 @@
 ﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,24 @@ namespace ApplicationCore.Interfaces
 {
     public interface IRepository<T> where T : BaseEntity
     {
+        Task<T?> GetByIdAsync(int id);
 
+        Task<List<T>> GetAllAsync();
+
+        Task<List<T>> GetAllAsync(ISpecification<T> specification);
+
+        Task<T> AddAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(T entity);
+
+        Task<int> CountAsync(ISpecification<T> specification); 
+        
+        Task<T> FirstAsync(ISpecification<T> specification);
+
+        Task<T?> FirstOrDefaultAsync(ISpecification<T> specification);
+
+        
     }
 }
